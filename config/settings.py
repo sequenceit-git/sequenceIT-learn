7978e637709c57a1d6e7097499b25cbab687cdd6
ovijitM <sdsufol@gmail.com>
2026-08-09 11:55:18 +0600

    fix: change WhiteNoise storage to CompressedStaticFilesStorage to prevent 500 error on missing manifest entries

--- config/settings.py
+++ config/settings.py
@@ -282,7 +282,7 @@ LOGGING = {
 }
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STUDENT_ID_PREFIX = config("STUDENT_ID_PREFIX", "ugr")
 LECTURER_ID_PREFIX = config("LECTURER_ID_PREFIX", "lec")
